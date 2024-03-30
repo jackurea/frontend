fb5e80dd082ccacd9ecd5f98120a06724c32d241
jackson <camilo05181997@gmail.com>
2024-03-29 17:38:55 -0700

    chore: design err

--- src/dialog/UserDelete.vue
+++ src/dialog/UserDelete.vue
@@ -28,9 +28,9 @@
         <template v-slot:default="{ isActive }">
             <v-card 
                 title="" 
-                style=" width: max-content !important; background: rgba(150,180,200,0.2); color: white; box-shadow: 0 2px 24px 10px rgba(16, 24, 91, 0.2),0 4px 5px 0 rgba(60,60,60,.14),0 1px 10px 0 rgba(60,60,60,.12)"
+                style=" width: max-content !important; border-radius:30px; padding: 10px; background:  linear-gradient(45deg, #000020, #000020); color: white; box-shadow: 0 2px 24px 10px rgba(16, 124, 150, 0.3),0 4px 10px 5px rgba(60,60,200,.14),0 1px 10px 0 rgba(60,60,60,.12)"
             >
-                <v-card-text style="white-space: nowrap; color: rgb(250,50,50,0.7); font-size: 22px; font-style: italic;">
+                <v-card-text style="white-space: nowrap; color: rgb(250,250,250,0.7); font-size: 22px; font-style: italic;">
                     Are you sure to delete this user?
                 </v-card-text>
 
